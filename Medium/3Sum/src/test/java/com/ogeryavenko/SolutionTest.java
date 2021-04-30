05c939628c5cca81fe5e3dda67084a5f5a7ce114
ogeryavenko <ogeryavenk@protonmail.com>
2021-04-30 18:10:02 +0300

    3Sum

---
+++ Medium/3Sum/src/test/java/com/ogeryavenko/SolutionTest.java
@@ -0,0 +1,16 @@
+package com.ogeryavenko;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class SolutionTest
+{
+    Solution solution = new Solution();
+    @Test
+    public void examle1()
+    {
+        
+    }
+}
